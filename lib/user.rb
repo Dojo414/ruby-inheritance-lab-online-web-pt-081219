4d7ba073216521286fe390bb61b9724beda2167e
Donovan Jones <donovanljones94@gmail.com>
2019-09-26 20:16:32 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,15 +1,4 @@
 class User 
   attr_reader :first_name, :last_name
-  @@all = []
   
-    def initialize
-      @first_name = first_name
-      @last_name = last_name
-      @@all << self
-    end
-    
-    def self.all
-      @@all
-    end
-    
   end
